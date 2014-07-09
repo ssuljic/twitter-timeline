73c4227d8250ccc0dd37fdb96f5fb0a909a0da49
ssuljic <sadzid.etf@gmail.com>
2014-07-09 13:40:42 +0200

    Removed credentials, refactored controller method

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,36 +1,14 @@
+require 'twitter_to_timeline'
+
 class TimelineController < ApplicationController
   def index
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = "42okgrA6b3EeGZls8VcWygfVi"
-      config.consumer_secret = "c3Cmb9phQa54SzSoNFme6WqTEEr2f59UrOk1nWB3pKaQeN62Nf"
+      config.consumer_key    = ""
+      config.consumer_secret = ""
     end
 
     my_tweets = client.user_timeline('sadzid_suljic')
-    @data = Hash.new
-    @data[:timeline] = Hash.new
-      @data[:timeline][:headline] = 'Timeline of my Tweets'
-      @data[:timeline][:type] = 'default'
-      @data[:timeline][:text] = 'Follow me on twitter @sadzid_suljic'
-      @data[:timeline][:asset] = Hash.new
-        @data[:timeline][:asset][:media] = 'https://pbs.twimg.com/profile_images/425408286804803584/96mV_bBa.jpeg'
-        @data[:timeline][:asset][:credit] = 'Sadzid Suljic'
-        @data[:timeline][:asset][:caption] = ''
-      @data[:timeline][:date] = Array.new
-      @data[:timeline][:era] = Array.new
-
-    my_tweets.each do |t|
-      d = Hash.new
-      d[:startDate] = '%s,%s,%s' % [t.created_at.year, t.created_at.month, t.created_at.day]
-      d[:endDate] = '%s,%s,%s' % [t.created_at.year, t.created_at.month, t.created_at.day]
-      d[:headline] = '@' + (t.retweet? ? t.retweeted_tweet.user.username : t.user.username)
-      d[:text] = ''
-      d[:asset] = Hash.new
-        d[:asset][:media] = t.url.to_s
-        d[:asset][:thumbnail] = t.retweet? ? t.retweeted_tweet.user.profile_image_url.to_s : t.user.profile_image_url.to_s
-        d[:asset][:credit] = ''
-        d[:asset][:caption] = ''
-      @data[:timeline][:date] << d
-    end
+    @data = TwitterToTimeline::adapt(my_tweets)
 
     respond_to do |format|
       format.html # index.html.erb
---
+++ lib/twitter_to_timeline.rb
@@ -0,0 +1,31 @@
+class TwitterToTimeline
+  def self.adapt(tweets)
+    data = Hash.new
+    data[:timeline] = Hash.new
+      data[:timeline][:headline] = 'Timeline of my Tweets'
+      data[:timeline][:type] = 'default'
+      data[:timeline][:text] = 'Follow me on twitter @sadzid_suljic'
+      data[:timeline][:asset] = Hash.new
+        data[:timeline][:asset][:media] = 'https://pbs.twimg.com/profile_images/425408286804803584/96mV_bBa.jpeg'
+        data[:timeline][:asset][:credit] = 'Sadzid Suljic'
+        data[:timeline][:asset][:caption] = ''
+      data[:timeline][:date] = Array.new
+      data[:timeline][:era] = Array.new
+
+    tweets.each do |t|
+      d = Hash.new
+      d[:startDate] = '%s,%s,%s' % [t.created_at.year, t.created_at.month, t.created_at.day]
+      d[:endDate] = '%s,%s,%s' % [t.created_at.year, t.created_at.month, t.created_at.day]
+      d[:headline] = '@' + (t.retweet? ? t.retweeted_tweet.user.username : t.user.username)
+      d[:text] = ''
+      d[:asset] = Hash.new
+        d[:asset][:media] = t.url.to_s
+        d[:asset][:thumbnail] = t.retweet? ? t.retweeted_tweet.user.profile_image_url.to_s : t.user.profile_image_url.to_s
+        d[:asset][:credit] = ''
+        d[:asset][:caption] = ''
+      data[:timeline][:date] << d
+    end
+
+    return data
+  end
+end
