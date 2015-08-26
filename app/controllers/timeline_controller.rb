62b0ec77007364179c80fa7c0aa0e233de6a00a1
ssuljic <sadzid.etf@gmail.com>
2015-08-26 21:01:36 +0200

    Removed cred

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,8 +3,8 @@ require 'twitter_to_timeline'
 class TimelineController < ApplicationController
   def index
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = "42okgrA6b3EeGZls8VcWygfVi"
-      config.consumer_secret = "c3Cmb9phQa54SzSoNFme6WqTEEr2f59UrOk1nWB3pKaQeN62Nf"
+      config.consumer_key    = ""
+      config.consumer_secret = ""
     end
 
     my_tweets = client.user_timeline('sadzid_suljic')
